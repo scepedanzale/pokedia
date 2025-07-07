c6e12afac759ba83dbb7654cd2d6e60afa25ae4f
Cielo <celesteartuso@hotmail.com>
2025-07-07 18:29:21 +0200

    refactor Loader component and styles for consistency

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -2,8 +2,8 @@ import pokeball_loader from '../imgs/pokeball_loader.png';
 
 export default function Loader() {
   return (
-    <div className='w-20 mx-auto'>
-      <img id='pokeball_loader' src={pokeball_loader} />
+    <div className='pokeball-loader-wrapper'>
+      <img id='pokeball-loader' src={pokeball_loader} />
     </div>
   )
 }
