6933fd21fe184acb7bf58b2d9252b0186c20e7c3
Cielo <celesteartuso@hotmail.com>
2025-10-08 10:54:43 +0200

    update homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,15 @@
+import Wrapper from "../components/layout/Wrapper";
 import PokemonList from "../components/pokemon/PokemonList";
 
-export default function HomePage() { return <PokemonList limit={50} /> }
+export default function HomePage() {
+
+    return (
+        <>
+            <Wrapper>
+                <h1>Welcome in PokédiA</h1>
+                <p>Explore the Pokémon world!</p>
+                <PokemonList limit={50} />
+            </Wrapper>
+        </>
+    )
+}
