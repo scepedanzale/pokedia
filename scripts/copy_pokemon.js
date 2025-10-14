6d84caa69713310e4aba2292371fc49e3c64487c
Cielo <celesteartuso@hotmail.com>
2025-10-14 23:59:44 +0200

    refactoring copy_pokemon: call to pokemon-specie

--- scripts/copy_pokemon.js
+++ scripts/copy_pokemon.js
@@ -1,13 +1,11 @@
 import axios from "axios";
 import fs from "fs";
 
-
 const BASE_URL = "https://pokeapi.co/api/v2";
 
 const fetchAllPokemon = () => {
-    return axios(BASE_URL + '/pokemon?limit=1000')
+    return axios(BASE_URL + '/pokemon?limit=10000')
         .then(response => {
-            console.log(response)
             return response.data.results;
         })
         .catch(err => {
@@ -15,68 +13,73 @@ const fetchAllPokemon = () => {
         })
 }
 
+
 const fetchDetail = async (pokemon) => {
     try {
         const pokeRes = await axios(pokemon.url)
         const poke = pokeRes.data
 
-        const specieRes = await axios(pokemon.url.replace('pokemon', 'pokemon-species'));
+        const specieRes = await axios(poke.species.url);
         const specie = specieRes.data
 
         const result = {
-            abilities: poke.abilities,
-            forms: poke.forms,
-            game_indices: poke.game_indices,
-            height: poke.height,
-            id: poke.id,
-            is_default: poke.is_default,
-            location_area_encounters: poke.location_area_encounters,
-            moves: poke.moves,
-            name: poke.name,
+            abilities: poke?.abilities,
+            forms: poke?.forms,
+            game_indices: poke?.game_indices,
+            height: poke?.height,
+            id: poke?.id,
+            is_default: poke?.is_default,
+            location_area_encounters: poke?.location_area_encounters,
+            moves: poke?.moves,
+            name: poke?.name,
             specie: {
-                base_happiness: specie.base_happiness,
-                capture_rate: specie.capture_rate,
-                color: specie.color,
-                egg_groups: specie.egg_groups,
-                evolution_chains: specie.evolution_chains,
-                evolves_from_species: specie.evolves_from_species,
-                form_descriptions: specie.form_descriptions,
-                forms_switchable: specie.forms_switchable,
-                gender_rate: specie.gender_rate,
-                genera: specie.genera,
-                generation: specie.generation,
-                habitat: specie.habitat,
-                has_gender_differences: specie.has_gender_differences,
-                hatch_counter: specie.hatch_counter,
-                is_baby: specie.is_baby,
-                is_legendary: specie.is_legendary,
-                is_mythical: specie.is_mythical,
-                name: specie.name,
-                shape: specie.shape,
-                varieties: specie.varieties,
+                base_happiness: specie?.base_happiness,
+                capture_rate: specie?.capture_rate,
+                color: specie?.color,
+                egg_groups: specie?.egg_groups,
+                evolution_chains: specie?.evolution_chains,
+                evolves_from_species: specie?.evolves_from_species,
+                form_descriptions: specie?.form_descriptions,
+                forms_switchable: specie?.forms_switchable,
+                gender_rate: specie?.gender_rate,
+                genera: specie?.genera,
+                generation: specie?.generation,
+                habitat: specie?.habitat,
+                has_gender_differences: specie?.has_gender_differences,
+                hatch_counter: specie?.hatch_counter,
+                is_baby: specie?.is_baby,
+                is_legendary: specie?.is_legendary,
+                is_mythical: specie?.is_mythical,
+                name: specie?.name,
+                shape: specie?.shape,
+                varieties: specie?.varieties,
             },
             sprites: {
-                front_default: poke.sprites?.front_default,
-                artwork_front_default: poke.sprites?.other['official-artwork']?.front_default,
-                animation: poke.sprites?.other?.showdown?.front_default
+                front_default: poke?.sprites?.front_default,
+                artwork_front_default: poke?.sprites?.other['official-artwork']?.front_default,
+                animation: poke?.sprites?.other?.showdown?.front_default
             },
-            stats: poke.stats,
-            types: poke.types,
-            weight: poke.weight
+            stats: poke?.stats,
+            types: poke?.types,
+            weight: poke?.weight
         }
-
-        console.log('RESULT', result);
 
         return result;
     } catch (err) {
-        console.log(err);
+        if (err.response?.status === 404) {
+            console.warn(`⚠️ Pokémon non trovato (${pokemon.name || pokemon.id})`);
+            return null;
+        } else {
+            console.error(`❌ Errore per ${pokemon.name || pokemon.id}:`, err.message);
+            return null;
+        }
     }
 }
 
 const main = async () => {
     const results = [];
     const allPokemon = await fetchAllPokemon();
-    console.log(allPokemon);
+    console.log(`Totale Pokémon trovati: ${allPokemon.length}`);
 
     const concurrency = 20;
 
